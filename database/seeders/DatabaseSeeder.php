45aa057c0506df0a2da421f7f743b2eefea955aa
vendywira <wayan.wiranatha@tech.jago.com>
2025-10-09 18:36:26 +0700

    create seeder for event

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Event;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Event::create([
+            'event_key' => 'rumah',
+            'location' => 'Br. Dinas Sudimara Kaja, Ds. Sudimara, Kec. Tabanan, Kab. Tabanan',
+            'event_date' => '2025-11-15',
+            'start_time' => '08:00',
+            'finish_time' => 'Selesai',
+            'google_map_link' => 'https://maps.app.goo.gl/XVJMwHX1okeuGJ5Q7'
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Event::create([
+            'event_key' => 'gedung',
+            'location' => 'Wedding Venue Taman Prakerti Bhuana Indraprasta Ballroom',
+            'event_date' => '2025-11-12',
+            'start_time' => '17:00',
+            'finish_time' => '21:00 WITA',
+            'google_map_link' => 'https://maps.app.goo.gl/gMvgt3TUPqrMAqRT7'
         ]);
     }
 }
